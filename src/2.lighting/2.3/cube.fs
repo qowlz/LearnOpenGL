a40374dcaf072e5a14c62740b4cfdbdf1771126a
byunguk.choi <qowlz45@gmail.com>
2023-04-22 17:18:43 +0900

    first commit

---
+++ src/2.lighting/2.3/cube.fs
@@ -0,0 +1,46 @@
+#version 330 core
+in vec3 FragPos;
+in vec3 Normal;
+
+out vec4 FragColor;
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
+uniform Material material;
+uniform vec3 viewPos;
+
+void main()
+{
+    // Ambient calculation
+    vec3 ambient = material.ambient * light.ambient;
+
+    // Diffuse calculation
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = (diff * material.diffuse) * light.diffuse;
+
+    // Specular calculation
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = (material.specular * spec) * light.specular;
+
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
